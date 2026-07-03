43979d4807402cbb24a828a0f5c123d8683fce40
Daniel Protopopov <daniel@scanblox.com>
2026-07-03 10:04:01 +0300

    SEO optimization

--- structs/var.go
+++ structs/var.go
@@ -1,11 +1,13 @@
 package structs
 
 import (
+	"github.com/nicksnyder/go-i18n/v2/i18n"
 	"github.com/redis/go-redis/v9"
 	"github.com/robfig/cron/v3"
 )
 
 var (
 	Redis     *redis.Client
+	Localizer *i18n.Localizer
 	Scheduler *cron.Cron
 )
