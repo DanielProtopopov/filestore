4b22ff033c696c14521f16424abdd27b392db22c
Daniel Protopopov <daniel@scanblox.com>
2026-07-07 11:32:34 +0300

    Using Redis event sink instead of cronjob to handle expired files

--- cmd/api/func.go
+++ cmd/api/func.go
@@ -2,44 +2,26 @@ package main
 
 import (
 	"context"
+	apiconfig "filestore/cmd/api/config"
+	redis2 "filestore/services/redis"
 	"filestore/structs"
 	"fmt"
-	"io/fs"
 	"log"
 	"os"
+	"slices"
 
 	"github.com/pkg/errors"
 	"github.com/redis/go-redis/v9"
 )
 
-func DeleteExpiredFiles(ctx context.Context, storagePath string) error {
-	directories, errReadStoragePath := os.ReadDir(storagePath)
-	if errReadStoragePath != nil {
-		return errors.Wrapf(errReadStoragePath, "Failed to read directory %s", storagePath)
+func DeleteExpiredFolder(ctx context.Context, folder string) error {
+	errRemoveDirectory := os.RemoveAll(fmt.Sprintf("%s/%s", apiconfig.Settings.StoragePath, folder))
+	if errRemoveDirectory != nil {
+		return errors.Wrapf(errRemoveDirectory, "Failed to remove directory %s: %s", folder, errRemoveDirectory.Error())
 	}
-
-	totalDirectoryCount := 0
-	var directoriesRemoved []string
-	for _, directory := range directories {
-		if !directory.IsDir() {
-			continue
-		}
-
-		keyExists, errKeyExists := structs.Redis.Exists(ctx, directory.Name()).Result()
-		if errKeyExists != nil && !errors.Is(errKeyExists, redis.Nil) {
-			log.Printf("[ERROR] Failed to check if %s exists: %s", directory.Name(), errKeyExists.Error())
-			continue
-		}
-
-		if keyExists <= 0 {
-			errRemoveDirectory := os.RemoveAll(fmt.Sprintf("%s/%s", storagePath, directory.Name()))
-			if errRemoveDirectory != nil {
-				log.Printf("[ERROR] Failed to remove directory %s: %s", directory.Name(), errRemoveDirectory.Error())
-				continue
-			}
-			directoriesRemoved = append(directoriesRemoved, directory.Name())
-			totalDirectoryCount++
-		}
+	_, errDelete := structs.Redis.Del(ctx, folder).Result()
+	if errDelete != nil {
+		return errors.Wrapf(errDelete, "Failed to delete Redis value by key %s: %s", folder, errDelete.Error())
 	}
 
 	redisIPEntries, errGetEntries := structs.Redis.Keys(ctx, "ip-*").Result()
@@ -50,36 +32,42 @@ func DeleteExpiredFiles(ctx context.Context, storagePath string) error {
 	for _, redisIPEntry := range redisIPEntries {
 		redisIPFolders, errGetRedisIPFolders := structs.Redis.LRange(ctx, redisIPEntry, 0, -1).Result()
 		if errGetRedisIPFolders != nil && !errors.Is(errGetRedisIPFolders, redis.Nil) {
-			return errors.Wrapf(errGetRedisIPFolders, "Failed to get a list of all IP address folders for %s", redisIPEntry)
+			return errors.Wrapf(errGetRedisIPFolders, "Failed to get a list of IP address folders for %s", redisIPEntry)
 		}
-		for _, redisIPFolder := range redisIPFolders {
-			_, errDirectoryExists := os.Stat(fmt.Sprintf("%s/%s", storagePath, redisIPFolder))
-			if errors.Is(errDirectoryExists, fs.ErrNotExist) {
-				errRemoveRedisIPFolder := structs.Redis.LRem(ctx, redisIPEntry, 0, redisIPFolder).Err()
-				if errRemoveRedisIPFolder != nil {
-					log.Printf("Failed to remove entry %s from Redis IP collection %s: %s", redisIPFolder, redisIPEntry, errRemoveRedisIPFolder.Error())
-					continue
-				}
-				ipFoldersCount, errGetCount := structs.Redis.LLen(ctx, redisIPEntry).Result()
-				if errGetCount != nil && !errors.Is(errGetCount, redis.Nil) {
-					log.Printf("Failed to get Redis IP folders collection count of %s: %s", redisIPEntry, errGetCount.Error())
-					continue
-				}
-				if ipFoldersCount == 0 {
-					_, errDeleteByKey := structs.Redis.Del(ctx, redisIPEntry).Result()
-					if errDeleteByKey != nil && !errors.Is(errDeleteByKey, redis.Nil) {
-						log.Printf("Failed to delete Redis IP collection by key %s: %s", redisIPEntry, errDeleteByKey.Error())
-						continue
-					}
-				}
-				_, errDelete := structs.Redis.Del(ctx, redisIPFolder).Result()
-				if errDelete != nil {
-					log.Printf("Failed to delete Redis value by key %s: %s", redisIPFolder, errDelete.Error())
-					continue
-				}
+
+		if slices.Contains(redisIPFolders, folder) {
+			_, errRemoveFolder := structs.Redis.LRem(ctx, redisIPEntry, 0, folder).Result()
+			if errRemoveFolder != nil && !errors.Is(errRemoveFolder, redis.Nil) {
+				return errors.Wrapf(errRemoveFolder, "Failed to remove folder %s from IP address set %s", folder, redisIPEntry)
 			}
 		}
 	}
 
 	return nil
+}
+
+// Start listening for events in Redis via Redis Event Sink
+func HandleRedisEvents(ctx context.Context) error {
+	const redisEventWorkers = 128
+	eventWorkers := make(chan struct{}, redisEventWorkers)
+
+	handleRedisEvent := func(ev *redis2.RedisEventMsg) {
+		if ev.KeyEvent == "expired" {
+			errDeleteExpiredFolder := DeleteExpiredFolder(ctx, ev.Key)
+			if errDeleteExpiredFolder != nil {
+				log.Printf("Failed to delete expired folder %s: %s", ev.Key, errDeleteExpiredFolder.Error())
+			}
+		}
+	}
+
+	for {
+		select {
+		case ev := <-apiconfig.Settings.EventSink.EventChannel:
+			eventWorkers <- struct{}{}
+			go func(event *redis2.RedisEventMsg) {
+				defer func() { <-eventWorkers }()
+				handleRedisEvent(event)
+			}(ev)
+		}
+	}
 }
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -7,13 +7,11 @@ import (
 	"filestore/internal/translations"
 	"filestore/structs"
 	"log"
-	"os"
 	"runtime/debug"
 	"time"
 
 	"github.com/joho/godotenv"
 	"github.com/nicksnyder/go-i18n/v2/i18n"
-	"github.com/robfig/cron/v3"
 )
 
 func main() {
@@ -26,21 +24,6 @@ func main() {
 	apiconfig.Init(ctx)
 	log.Println("Configuration was successfully loaded")
 
-	structs.Scheduler = cron.New(cron.WithChain(cron.SkipIfStillRunning(cron.VerbosePrintfLogger(log.New(os.Stdout, "cron: ", log.LstdFlags)))))
-
-	var standardParser = cron.NewParser(cron.Second | cron.Minute | cron.Hour | cron.Dom | cron.Month | cron.DowOptional | cron.Descriptor)
-	storageCheckSched, errParseStorageDuration := standardParser.Parse("*/30 * * * * *")
-	if errParseStorageDuration != nil {
-		log.Panicf("Error parsing scheduler for storage check: %s", errParseStorageDuration.Error())
-	}
-
-	structs.Scheduler.Schedule(storageCheckSched, cron.FuncJob(func() {
-		errDeleteExpiredFiles := DeleteExpiredFiles(ctx, apiconfig.Settings.StoragePath)
-		if errDeleteExpiredFiles != nil {
-			log.Printf("[ERROR] Failed to check files due for deletion: %s", errDeleteExpiredFiles.Error())
-		}
-	}))
-
 	go func() {
 		t := time.Tick(time.Minute)
 		for {
@@ -51,13 +34,8 @@ func main() {
 
 	structs.Localizer = i18n.NewLocalizer(apiconfig.Settings.Bundle, translations.DefaultLanguage)
 
-	if len(structs.Scheduler.Entries()) != 0 {
-		structs.Scheduler.Start()
-	}
+	// Process Redis events (to replace cron job)
+	go HandleRedisEvents(ctx)
 
-	errDeleteExpiredFiles := DeleteExpiredFiles(ctx, apiconfig.Settings.StoragePath)
-	if errDeleteExpiredFiles != nil {
-		log.Printf("[ERROR] Failed to check files due for deletion: %s", errDeleteExpiredFiles.Error())
-	}
 	api.Serve()
 }
